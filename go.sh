8ec07e48a77189456933f49e5dd25fe60085f8e4
saildrone.workspace <saildrone.workspace>
2023-07-06 22:58:44 +0000

    add a comment

--- go.sh
+++ go.sh
@@ -1,3 +1,4 @@
+# launch workes locally for bacground task testing.
 celery -A app:celery_app worker --loglevel=DEBUG --concurrency=8 &
 celery -A app:celery_app beat &
 /app/.heroku/python/bin/python /workspace/app.py
